565c49886b436b1a09a44f8da7c6d42d85283547
ImeldhaOctaVinanda <imeldhaocta35@gmail.com>
2024-11-01 12:48:30 +0700

    update

---
+++ src/components/MainLayout/Footer.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-footer dark padless>
+      <v-card flat tile class="secondary white--text text-center">
+        <v-card-text>
+          <v-btn
+            v-for="(icon, i) in icons"
+            :key="i"
+            class="mx-4 white--text"
+            :href="icon.link"
+            target="_blank"
+            icon
+          >
+            <v-icon size="24px">{{ icon.text }}</v-icon>
+          </v-btn>
+        </v-card-text>
+  
+        <v-card-text class="white--text pt-0">
+          How long are you going to wait before you demand the best for yourself?
+        </v-card-text>
+  
+        <v-divider></v-divider>
+  
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>PT Pucink Max
+  
+          </strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </template>
+  
+  <style scoped>
+  .v-card {
+    width: 100%;
+  }
+  </style>
+  
+  <script>
+  export default {
+    data: () => ({
+      icons: [
+        {
+          text: "mdi-whatsapp",
+          link: "http://wa.me/6281548191447",
+        },
+        {
+          text: "mdi-instagram",
+          link: "https://www.instagram.com/healthyfood_bymamamika?igsh=d2Y3eHZjcmUzeWoy",
+        },
+      ],
+    }),
+  };
+  </script>
---
+++ src/components/MainLayout/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header data-layername="navbar" class="flex flex-col text-xs tracking-tight text-black max-w-[697px]">
+      <nav class="flex flex-wrap gap-5 justify-between ml-5 w-full max-w-[606px] max-md:max-w-full">
+        <div class="flex gap-5">
+          <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/24735e51dec2cf3a2e820fa5539395afc5ed5d3298e4d5a9acbcf783027602d4?placeholderIfAbsent=true&apiKey=b642bcfadf164c35aebff8fb97b04ffb" class="object-contain shrink-0 w-8 aspect-[1.1] rounded-[100000px]" alt="Company logo" />
+          <ul class="flex gap-4 justify-center items-center my-auto">
+            <li data-layername="logo" class="self-stretch my-auto text-xs font-bold">
+              <a href="/" class="focus:outline-none focus:underline">Home</a>
+            </li>
+            <li data-layername="shop" class="self-stretch my-auto">
+              <a href="/about" class="focus:outline-none focus:underline">About Us</a>
+            </li>
+            <li data-layername="shop" class="self-stretch my-auto">
+              <a href="/menu" class="focus:outline-none focus:underline">Menu</a>
+            </li>
+          </ul>
+        </div>
+        <button data-layername="login" class="gap-3 my-auto whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-black">
+          Login
+        </button>
+      </nav>
+      <div class="w-full border-black min-h-[1px] max-md:max-w-full" role="separator"></div>
+    </header>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'Navbar',
+  });
+  </script>
